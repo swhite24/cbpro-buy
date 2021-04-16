0b1c7a2021312b80df959ad71cf14dd2bbeea2e5
Steven White <swhitewvu24@gmail.com>
2021-04-16 00:44:15 -0400

    add basis config options

--- pkg/cmd/cbprobuy.go
+++ pkg/cmd/cbprobuy.go
@@ -20,13 +20,16 @@ func init() {
 	var currency, product string
 	var useCoinbase, useSandbox, autoDeposit bool
 	var amount float64
+	var useBasis bool
+	var basisWindowStart, basisMultiplier float64
 
 	CBProBuyCmd = &cobra.Command{
 		Use:   "cbpro-buy",
 		Short: "cbpro-buy purchases crypto from coinbase pro with auto deposit",
 		Run: func(cmd *cobra.Command, args []string) {
 			cfg := config.InitializeConfig(cmd.Flags())
-			err := purchase.InitiatePurchase(initializeClient(cfg), cfg)
+			client := initializeClient(cfg)
+			err := purchase.InitiatePurchase(client, cfg)
 			if err != nil {
 				fmt.Println("failed to purchase")
 				fmt.Println(err)
@@ -47,6 +50,9 @@ func init() {
 	CBProBuyCmd.Flags().BoolVar(&useSandbox, "sandbox", false, "Whether to use coinbase pro sandbox environment (will require different api key")
 	CBProBuyCmd.Flags().BoolVar(&autoDeposit, "autodeposit", false, "Whether to auto deposit funds if current account is less than amount")
 	CBProBuyCmd.Flags().Float64Var(&amount, "amount", 50, "Amount of product to purchase")
+	CBProBuyCmd.Flags().BoolVar(&useBasis, "use-basis", false, "Whether to adjust purchase amount if current price is below average cost over time window")
+	CBProBuyCmd.Flags().Float64Var(&basisWindowStart, "basis-window-start", 30, "Mumber of days in the past to for beginning of basis window")
+	CBProBuyCmd.Flags().Float64Var(&basisMultiplier, "basis-multiplier", 1.5, "Scale to apply to purchase amount if current price is less than average cost")
 }
 
 func initializeClient(cfg *config.Config) *coinbasepro.Client {
